438f84e78d4bef20db36877b87e1884634acd654
Joao Victor <falejoaovictor@hotmail.com>
2021-05-06 13:11:15 -0300

    adicionado métodos privados

--- conta.py
+++ conta.py
@@ -11,21 +11,30 @@ class Conta:
     
     def extrato(self):
         print("Saldo {} do titular {}".format(self.__saldo, self.__titular))
-    
+
     def deposita(self, valor):
         self.__saldo += valor
 
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_a_sacar = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel_a_sacar
+
     def saca(self, valor):
-        self.__saldo -= valor
+        if self.__pode_sacar(valor):
+            self.__saldo -= valor
+        else:
+            print("o valor {} passou o limite".format(valor))
 
     def transfere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
 
-    def get_saldo(self):
+    @property
+    def saldo(self):
         return self.__saldo
 
-    def get_titular(self):
+    @property
+    def titular(self):
         return self.__titular
 
     @property
